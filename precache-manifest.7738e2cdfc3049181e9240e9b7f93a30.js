c9a9bad9ef1b57987a84097ebe83bec372d0ee79
suian <1084031114@qq.com>
2021-07-26 00:01:42 +0800

    deploy

---
+++ precache-manifest.7738e2cdfc3049181e9240e9b7f93a30.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72d05e50c3c58242e6f2",
+    "url": "/Vue-Cnode-Web/css/app.8db8183f.css"
+  },
+  {
+    "revision": "5c422f48589299b6d89b",
+    "url": "/Vue-Cnode-Web/css/chunk-vendors.37b3d8f8.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/Vue-Cnode-Web/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/Vue-Cnode-Web/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "48f4b8b6fba484d9b8bf08b51d560402",
+    "url": "/Vue-Cnode-Web/img/cnodejs.48f4b8b6.svg"
+  },
+  {
+    "revision": "d95dc0f44202f2665e112c25e90477ac",
+    "url": "/Vue-Cnode-Web/index.html"
+  },
+  {
+    "revision": "72d05e50c3c58242e6f2",
+    "url": "/Vue-Cnode-Web/js/app.2e3d917f.js"
+  },
+  {
+    "revision": "5c422f48589299b6d89b",
+    "url": "/Vue-Cnode-Web/js/chunk-vendors.87a81172.js"
+  },
+  {
+    "revision": "b9e4b4c3ab90de762cabe41cc9eafa6a",
+    "url": "/Vue-Cnode-Web/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/Vue-Cnode-Web/robots.txt"
+  }
+]);
